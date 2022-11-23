1ee5752fb385698248a85aff0733a21dfb02b8f1
Junior Marques <junior.marques@aluno.ifsp.edu.br>
2022-11-23 20:05:17 -0300

    correcao db.go

--- database/db.go
+++ database/db.go
@@ -18,7 +18,7 @@ func ConectaComBancoDeDados() {
 	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+
 	" password="+os.Getenv("PASSWORD")+
 	"dbname="+os.Getenv("DBNAME")+
-	" port="+os.Getenv(" DBPORT")+" sslmode=disable"
+	"dbport="+os.Getenv("DBPORT")+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
